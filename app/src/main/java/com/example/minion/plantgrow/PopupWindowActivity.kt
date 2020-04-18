272db292ba99feeef51ec01207fd1baf0e4eaef8
MinionCZECH <43399650+MinionCZ@users.noreply.github.com>
2020-04-18 11:18:21 +0200

    18/04 - popup windows started

--- app/src/main/java/com/example/minion/plantgrow/PopupWindowActivity.kt
+++ app/src/main/java/com/example/minion/plantgrow/PopupWindowActivity.kt
@@ -2,10 +2,15 @@ package com.example.minion.plantgrow
 
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.DisplayMetrics
+import android.view.Gravity
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import android.widget.SeekBar
+import android.widget.TextView
 
 class PopupWindowActivity : AppCompatActivity() {
     private var index = 0
@@ -15,6 +20,7 @@ class PopupWindowActivity : AppCompatActivity() {
     private var height = 0
     private var width = 0
     private var dayString = ""
+    private lateinit var ledInfo: LedInfo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.popup_window_layout)
@@ -26,13 +32,11 @@ class PopupWindowActivity : AppCompatActivity() {
         height = displayMetrics.heightPixels
         width = displayMetrics.widthPixels
         setPopupStyle()
+        ledInfo = LedMainInfo.getInstance().getLedInfo(index)
     }
 
     private fun setPopupStyle() {
-        var root = RelativeLayout(applicationContext)
         window.setLayout(width*9/10,height/3)
-
-
         var gradientDrawable = GradientDrawable()
         gradientDrawable.cornerRadius = 50F
         gradientDrawable.setColor(Color.DKGRAY)
@@ -40,7 +44,33 @@ class PopupWindowActivity : AppCompatActivity() {
         gradientDrawable.setSize(width*9/10, height/3)
         window.setBackgroundDrawable(gradientDrawable)
         mainLayout.background = gradientDrawable
-        root.background = gradientDrawable
-        mainLayout.addView(root)
     }
+    private fun createSliderLayout(isBlue:Boolean):LinearLayout{
+        var root = LinearLayout(applicationContext)
+        var relativeLayout = RelativeLayout(applicationContext)
+        var seekBar : SeekBar = SeekBar(applicationContext)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            seekBar.min = 0
+        }
+        seekBar.max = 1000
+        seekBar.progress = ledInfo.getBlueLedPower(dayIndex).toInt()
+        seekBar.minimumWidth = width*3/4
+        var ledName = if (isBlue) createTextView("Modrá LED: ") else createTextView("Červená LED: ")
+
+
+
+return root
+
+    }
+private fun createTextView(text:String):TextView{
+    var textView = TextView(applicationContext)
+    textView.text = text
+    textView.gravity = Gravity.LEFT
+    textView.textSize = 20f
+    textView.setTextColor(Color.BLACK)
+    return textView
+}
+
+
+
 }
